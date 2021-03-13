517ea4e9dda597cf26aa1769d8d96d7392ecf333
KIMSG <yuiox17@gmail.com>
2021-03-13 12:10:53 +0900

    해설..

--- case1_context-menu/solution/1.presenter/q1_js/sol_index.js
+++ case1_context-menu/solution/1.presenter/q1_js/sol_index.js
@@ -10,7 +10,8 @@ items.forEach(function(item) {
     });
   });
 });
-
+// 자바스크립트가 모든 이벤트를 실시간으로 감지하는 형태이기 째문에
+// 더 많은 리스트가 생길 수록 비효율적이고 속도가 느려지는 코드다.
 
 
 // 2. 더 나은 방법 (1)
@@ -19,6 +20,7 @@ const items = document.querySelectorAll('.item');
 wrapper.addEventListener('click', function(e) {
   const targetElem = e.target;
   e.stopPropagation();
+  // 이거는 버블링을 없애주는 건데요. 기억 해두세요.
   if (!targetElem.classList.contains('item')) return;
   targetElem.classList.toggle('open');
   items.forEach(function(elem) {
@@ -60,18 +62,18 @@ document.body.addEventListener('click', function(e) {
 /**
  * <해설>
 
-- 초보자들은 DOM 제어와 관련하여 이벤트 리스너를 잔뜩 작성하곤 한다. 목록의 각 아이템 하나하나마다 등록하는 식이다. 본 문제의 경우를 예로 들면 다음과 같이 
+- 초보자들은 DOM 제어와 관련하여 이벤트 리스너를 잔뜩 작성하곤 한다. 목록의 각 아이템 하나하나마다 등록하는 식이다. 본 문제의 경우를 예로 들면 다음과 같이
 
 items.forEach(function(item) {
   item.addEventListener(‘click’, …)
 }
 
-이렇게 작성하면 크게 두가지 문제가 있다. 
+이렇게 작성하면 크게 두가지 문제가 있다.
 
-(1) 이벤트 감시대상이 많은 만큼 메모리에 부담이 된다. 
+(1) 이벤트 감시대상이 많은 만큼 메모리에 부담이 된다.
 (2) 어떤 변경에 의해 목록에 아이템이 추가될 경우 해당 아이템은 감시대상에 속하지 않아 팝오버 동작이 이뤄지지 않는다. 따라서 새로운 아이템이 추가될 때마다 그에 대한 리스너를 등록해주어야 한다.
 
-반면 리스너를 상위 노드에 등록하면 위 두가지 문제가 모두 해결된다. 따라서 리스너 등록은 최소화하는 것이 바람직하다. 
+반면 리스너를 상위 노드에 등록하면 위 두가지 문제가 모두 해결된다. 따라서 리스너 등록은 최소화하는 것이 바람직하다.
 
 이벤트 핸들러를 최소화하기 위해서는 캡쳐링/버블링을 이해하는 것이 필요하다. 나아가 리스너 함수의 첫번째 인자인 event 객체의 내부에 어떤 정보가 들어있는지를 알 필요가 있다.
 
@@ -84,5 +86,5 @@ items.forEach(function(item) {
 단점: 1) 함수 내부에 등장할수밖에 없는 조건문에 대한 최적화 필요.
       2) 개별 등록/해제 가능한 리스너에 비해 관리가 어려움
 
-* 
+*
  */
